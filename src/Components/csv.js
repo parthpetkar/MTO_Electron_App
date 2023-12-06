adae0878d106e86d9a19414f69122ed6b19f1869
parthpetkar <callofdutyparth2004@gmail.com>
2023-12-06 13:42:09 +0530

    Data preprocessing

--- src/Components/csv.js
+++ src/Components/csv.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const XlsxFileUploader = () => {
     const [downloadLink, setDownloadLink] = React.useState('');
+    const [uploadMessage, setUploadMessage] = React.useState('');
 
     const handleFileChange = (event) => {
         const file = event.target.files[0];
@@ -20,21 +21,28 @@ const XlsxFileUploader = () => {
             method: 'POST',
             body: formData,
         })
-            .then((response) => response.text())
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`File upload failed: ${response.statusText}`);
+                }
+                return response.text();
+            })
             .then((message) => {
                 console.log(message);
-                // Set the download link for the button
                 setDownloadLink('http://localhost:5000/download');
+                setUploadMessage('File uploaded successfully!');
             })
             .catch((error) => {
                 console.error('Error uploading file:', error);
+                setUploadMessage('Error uploading file. Please try again.');
             });
     };
 
     return (
         <div>
-            <h2>XLSX File Uploader</h2>
+            <h2>CSV File Uploader</h2>
             <input type="file" onChange={handleFileChange} />
+            {uploadMessage && <p>{uploadMessage}</p>}
             {downloadLink && (
                 <div>
                     <a href={downloadLink} download="downloaded_file.xlsx">
--- src/app.py
+++ src/app.py
@@ -1,28 +1,61 @@
 from flask import Flask, request, send_file
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 import os
 
 app = Flask(__name__)
+
+def calculate_total_quantity(file_path):
+    # Read the CSV file
+    data = pd.read_csv(file_path)
+
+    # Label encoding for categorical columns
+    label_encoder_service = LabelEncoder()
+    data['SERVICE'] = label_encoder_service.fit_transform(data['SERVICE'])
+
+    label_encoder_size1 = LabelEncoder()
+    data['SIZE 1'] = label_encoder_size1.fit_transform(data['SIZE 1'])
+
+    label_encoder_size2 = LabelEncoder()
+    data['SIZE 2'] = label_encoder_size2.fit_transform(data['SIZE 2'])
+
+    label_encoder_item = LabelEncoder()
+    data['ITEM'] = label_encoder_item.fit_transform(data['ITEM'])
+
+    # Group by the specified columns and sum the quantities
+    grouped_data = data.groupby(['SERVICE', 'SIZE 1', 'SIZE 2', 'ITEM'])['QTY'].sum().reset_index()
+
+    # Save the grouped data to a temporary CSV file
+    temp_grouped_csv_path = 'temp_grouped.csv'
+    grouped_data.to_csv(temp_grouped_csv_path, index=False)
+
+    return temp_grouped_csv_path, label_encoder_service, label_encoder_size1, label_encoder_size2, label_encoder_item
+
+def reverse_encode(data, label_encoder_service, label_encoder_size1, label_encoder_size2, label_encoder_item):
+    # Reverse label encoding for categorical columns
+    data['SERVICE'] = label_encoder_service.inverse_transform(data['SERVICE'])
+    data['SIZE 1'] = label_encoder_size1.inverse_transform(data['SIZE 1'])
+    data['SIZE 2'] = label_encoder_size2.inverse_transform(data['SIZE 2'])
+    data['ITEM'] = label_encoder_item.inverse_transform(data['ITEM'])
+
+    return data
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     try:
         file = request.files['file']
 
-        # Save the XLSX file temporarily
-        temp_xlsx_path = 'temp.xlsx'
-        file.save(temp_xlsx_path)
+        # Assuming the uploaded file is a CSV, you may want to validate this
+        data = pd.read_csv(file)
 
-        # Convert XLSX to CSV
+        # Save the data to a temporary CSV file
         temp_csv_path = 'temp.csv'
-        data = pd.read_excel(temp_xlsx_path)
-        data = pd.read_excel(temp_xlsx_path, converters={'SIZE 1': int, 'SIZE 2': int, 'QTY': int})
         data.to_csv(temp_csv_path, index=False)
 
-        # Clean up temporary XLSX file
-        os.remove(temp_xlsx_path)
+        # Calculate total quantity and save the grouped data to a temporary CSV file
+        temp_grouped_csv_path, label_encoder_service, label_encoder_size1, label_encoder_size2, label_encoder_item = calculate_total_quantity(temp_csv_path)
 
-        return 'File uploaded successfully!'
+        return f'File uploaded successfully! Grouped data saved to: {temp_grouped_csv_path}'
     except Exception as e:
         return f'Error: {str(e)}'
 
@@ -30,18 +63,19 @@ def upload_file():
 def download_file():
     try:
         # Convert CSV to XLSX
-        temp_csv_path = 'temp.csv'
+        temp_grouped_csv_path, label_encoder_service, label_encoder_size1, label_encoder_size2, label_encoder_item = calculate_total_quantity('temp.csv')
         temp_xlsx_path = 'downloaded_file.xlsx'
-        data = pd.read_csv(temp_csv_path)
-        data.to_excel(temp_xlsx_path, index=False)
+        grouped_data = pd.read_csv(temp_grouped_csv_path)
+
+        # Reverse label encoding before saving to XLSX
+        grouped_data_reverse = reverse_encode(grouped_data.copy(), label_encoder_service, label_encoder_size1, label_encoder_size2, label_encoder_item)
+
+        grouped_data_reverse.to_excel(temp_xlsx_path, index=False)
 
         # Send the XLSX file for download
         return send_file(temp_xlsx_path, as_attachment=True, download_name='downloaded_file.xlsx')
     except Exception as e:
         return f'Error: {str(e)}'
-    finally:
-        # Clean up temporary CSV file
-        os.remove(temp_csv_path)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
